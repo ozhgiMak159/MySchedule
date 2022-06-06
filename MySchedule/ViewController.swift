49fcbbef95ee4cc67aa098e50cf8b05b2ce2853d
Maksim Ozhgibecov <93679282+ozhgiMak159@users.noreply.github.com>
2022-06-06 14:47:33 +0500

    Initial Commit

---
+++ MySchedule/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySchedule
+//
+//  Created by Maksim  on 06.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
