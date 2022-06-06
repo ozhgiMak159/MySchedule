47145dc6a5b3abbce09b1c2f32bce90e3bed5f00
Maksim Ozhgibecov <93679282+ozhgiMak159@users.noreply.github.com>
2022-06-06 16:01:51 +0500

    1

--- MySchedule/Controllers/ScheduleVC.swift
+++ MySchedule/Controllers/ScheduleVC.swift
@@ -7,13 +7,71 @@
 
 
 import UIKit
+import FSCalendar
+
+
+// Закончил на 3 видео - 9:10
 
 class ScheduleViewController: UIViewController {
     
+    private let calendar: FSCalendar = {
+        let calendar = FSCalendar()
+        calendar.translatesAutoresizingMaskIntoConstraints = false
+        return calendar
+    }()
+    
+    private var calendarHeightConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        addSubView()
+        setConstrains()
+    }
+    
+    private func setDelegate() {
+        
+    }
+    
+    
+    
+}
+extension ScheduleViewController: FSCalendarDelegate {
+ 
+
+}
+
+extension ScheduleViewController: FSCalendarDataSource {
+ 
+
+}
+
+
+
+
+
+extension ScheduleViewController {
+    
+    private func addSubView() {
         view.backgroundColor = .white
         title = "Schedule"
+        
+        view.addSubview(calendar)
+        
+        calendarHeightConstraint = NSLayoutConstraint(
+        item: calendar, attribute: .height, relatedBy: .equal,
+        toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 300)
+        calendar.addConstraint(calendarHeightConstraint)
+    
     }
+    
+    
+    private func setConstrains() {
+        NSLayoutConstraint.activate([
+            calendar.topAnchor.constraint(equalTo: view.topAnchor, constant: 90),
+            calendar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            calendar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+        ])
+    
+    }
+
 }
